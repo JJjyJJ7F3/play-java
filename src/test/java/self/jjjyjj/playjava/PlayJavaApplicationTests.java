4b0cda4d3c885688f380b030c2dc4a59bd82a1f0
MegaLeap <zhang_jian_work@hotmail.com>
2024-12-29 15:34:27 +0800

    add more items in gitignore

--- src/test/java/self/jjjyjj/playjava/PlayJavaApplicationTests.java
+++ src/test/java/self/jjjyjj/playjava/PlayJavaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PlayJavaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
