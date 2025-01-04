3b1e86a0d8012ed470bf8d7011915df169815820
MegaLeap <zhang_jian_flow@hotmail.com>
2025-01-04 21:27:28 +0800

    example that initiate instance with  telescoping constructor pattern

---
+++ basics/src/main/java/self/jjjyjj/effective/item2/NutritionFactsDemo.java
@@ -0,0 +1,10 @@
+package self.jjjyjj.effective.item2;
+
+public class NutritionFactsDemo {
+    public static void main(String[] args) {
+        NutritionFacts nutritionFacts = new NutritionFacts(240, 100, 20,
+                10, 10, 12);
+
+
+    }
+}
